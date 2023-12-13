89e3651106e82fe32d87cfdd4f5ce8e0d21299cd
JulianaAmoasei <late.parrot@gmail.com>
2023-12-13 14:28:44 -0300

    video: média com for...of

---
+++ for-of.js
@@ -0,0 +1,11 @@
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+for (let nota of notas) {
+  somaDasNotas += nota;
+}
+
+const media = somaDasNotas / notas.length;
+
+console.log(`a média das notas é ${media}`);
