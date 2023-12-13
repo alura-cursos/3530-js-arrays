a0896ab44e4956790bdf2d08a9c31d4d4513531e
JulianaAmoasei <late.parrot@gmail.com>
2023-12-13 11:40:28 -0300

    video: alterando com splice

---
+++ atualizar-lista.js
@@ -0,0 +1,6 @@
+const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
+
+listaEstudantes.splice(1, 2, 'Rodrigo');
+// listaEstudantes.push('Rodrigo');
+
+console.log(listaEstudantes);
