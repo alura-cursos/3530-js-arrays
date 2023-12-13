647360b01bf84255d66ebcaa0ccbc7f0ec0f5559
JulianaAmoasei <late.parrot@gmail.com>
2023-12-13 12:08:54 -0300

    video: array de duas dimensões

---
+++ array-de-arrays.js
@@ -0,0 +1,12 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const medias = [10, 8, 7.5, 9];
+
+//              []   ,  [] 
+const lista = [alunos, medias];
+
+console.log(
+  `a aluna da posição 1 da lista é: ${lista[0][1]}.
+  a nota dessa aluna é ${lista[1][1]}`
+);
+
+console.log(lista[0]);
