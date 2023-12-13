0540c3d84f5a8d6c2454b8ed6a8b7f56e5b5ff86
JulianaAmoasei <late.parrot@gmail.com>
2023-12-13 11:06:29 -0300

    video: dividindo com slice

---
+++ dividir-array.js
@@ -0,0 +1,7 @@
+const listaEstudantes = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'André', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
+
+const sala1 = listaEstudantes.slice(0, listaEstudantes.length/2);
+const sala2 = listaEstudantes.slice(listaEstudantes.length/2);
+
+console.log(sala1);
+console.log(sala2);
