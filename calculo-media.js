c07a7ad7aae9c40e994f38d2b7ccd2a0f7af6630
JulianaAmoasei <late.parrot@gmail.com>
2023-12-13 10:22:19 -0300

    video: entendendo arrays

---
+++ calculo-media.js
@@ -0,0 +1,10 @@
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+
+const notas = [10, 6.5, 8, 7.5];
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+
+console.log(media);
