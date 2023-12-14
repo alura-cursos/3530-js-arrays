ad39b97cc40f589efea0f502439d561cb52a0850
JulianaAmoasei <late.parrot@gmail.com>
2023-12-14 15:58:48 -0300

    video: removendo elementos com Set

---
+++ set.js
@@ -0,0 +1,8 @@
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+// const nomesAtualizados = new Set(nomes);
+
+const listaNomesAtualizados = [...new Set(nomes)];
+
+// console.log(nomesAtualizados);
+console.log(listaNomesAtualizados);
