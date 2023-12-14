f9103b04026b82665a07c4b6332098dfd226aeca
JulianaAmoasei <late.parrot@gmail.com>
2023-12-14 15:47:29 -0300

    video: clonando arrays

---
+++ clonar.js
@@ -0,0 +1,13 @@
+const notas = [7, 7, 8, 9];
+const novaListaNotas = [...notas, 10];
+
+// spread operator
+// novaListaNotas.push(10);
+
+// let nota = 9.75;
+// let novaNota = nota;
+
+// nota = 10;
+
+console.log(notas);
+console.log(novaListaNotas);
